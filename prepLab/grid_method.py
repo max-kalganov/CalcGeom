d8b3b469bf6e542a82e3caf6fa0e5c7109409c91
max <maksim.kalganov@kreo.net>
2020-03-04 16:07:58 +0300

    rewrite grid method

--- prepLab/grid_method.py
+++ prepLab/grid_method.py
@@ -1,5 +1,5 @@
 import random
-
+from math import ceil, floor
 from Models import Dot
 from ct import CANVAS_WIDTH, CANVAS_HEIGHT
 from visualizer import Visualizer
@@ -7,51 +7,90 @@ from visualizer import Visualizer
 
 class GridMethod:
     def __init__(self):
-        self.number_of_points =250000
-        self.windows_width = 50
+        self.number_of_points =100000
+        self.windows_width = 10
         self.windows = [[[] for j in range(CANVAS_WIDTH//self.windows_width + 1)] for i in range(CANVAS_HEIGHT//self.windows_width + 1)]
         self.colored_points = []
 
     def init_points_function(self, obj: Visualizer):
+        seen = {}
         for i in range(self.number_of_points):
             x = random.randint(0, CANVAS_WIDTH)
             y = random.randint(0, CANVAS_HEIGHT)
+            if x in seen:
+                if y not in seen[x]:
+                    seen[x].add(y)
+                else:
+                    continue
+            else:
+                seen.setdefault(x, {y})
+
             point = obj.canv.create_rectangle((x, y) * 2)
             self.windows[y//self.windows_width][x//self.windows_width].append(point)
+
+    def decorate_all_points(self, vis, old_points, points):
+        for point in points:
+            if point in old_points:
+                old_points.remove(point)
+            else:
+                vis.canv.itemconfig(point, outline="#ff0000")
+            self.colored_points.append(point)
+
+    def check_and_decorate_points(self, vis, old_points, second_type_windows, min_x, max_x, min_y, max_y):
+        for point in second_type_windows:
+            coords = vis.canv.coords(point)
+            if min_x <= coords[0] <= max_x and min_y <= coords[1] <= max_y:
+                if point in old_points:
+                    old_points.remove(point)
+                else:
+                    vis.canv.itemconfig(point, outline="#ff0000")
+                self.colored_points.append(point)
+
+    def check_first_and_last_window(self, vis, old_points, line, min_x, max_x, min_y, max_y):
+        if ceil(min_x / self.windows_width) != min_x / self.windows_width:
+            self.check_and_decorate_points(vis, old_points, line[floor(min_x / self.windows_width)],
+                                           min_x, max_x, min_y, max_y)
+
+        if floor(max_x / self.windows_width) != max_x / self.windows_width:
+            self.check_and_decorate_points(vis, old_points, line[ceil(max_x / self.windows_width)],
+                                           min_x, max_x, min_y, max_y)
+
+    def check_first_or_last_line(self, line_num, vis, old_points, min_x, max_x, min_y, max_y):
+        for border_windows in self.windows[line_num]:
+            for sec_type_window in border_windows[
+                                   ceil(min_x / self.windows_width):floor(max_x / self.windows_width) + 1]:
+                self.check_and_decorate_points(vis, old_points, sec_type_window,
+                                               min_x, max_x, min_y, max_y)
+
+            self.check_first_and_last_window(vis, old_points, border_windows, min_x, max_x, min_y, max_y)
 
     def action_decor(self, vis: Visualizer):
         def action(event):
             # probably you should improve this coloring
-            for point in self.colored_points:
-                vis.canv.itemconfig(point, outline="#000000")
+            old_points = set(self.colored_points)
             self.colored_points = []
 
-            for lines in self.windows[min_y//self.windows_width+1:max_y//self.windows_width]:
-                for points_in_window in lines[min_x//self.windows_width+1:max_x//self.windows_width]:
-                    for point in points_in_window:
-                        vis.canv.itemconfig(point, outline="#ff0000")
-                        self.colored_points.append(point)
-                for points_in_window in [lines[min_x//self.windows_width], lines[max_x//self.windows_width]]:
-                    for point in points_in_window:
-                        coords = vis.canv.coords(point)
-                        if min_x <= coords[0] <= max_x and min_y <= coords[1] <= max_y:
-                            vis.canv.itemconfig(point, outline="#ff0000")
-                            self.colored_points.append(point)
+            for line in self.windows[ceil(min_y / self.windows_width):floor(max_y / self.windows_width) + 1]:
+                for first_type_window in line[ceil(min_x / self.windows_width):floor(max_x / self.windows_width) + 1]:
+                    self.decorate_all_points(vis, old_points, first_type_window)
 
-            for border_lines in [self.windows[min_y//self.windows_width], self.windows[max_y//self.windows_width]]:
-                for points_in_window in border_lines[min_x//self.windows_width+1:max_x//self.windows_width]:
-                    for point in points_in_window:
-                        coords = vis.canv.coords(point)
-                        if min_x <= coords[0] <= max_x and min_y <= coords[1] <= max_y:
-                            vis.canv.itemconfig(point, outline="#ff0000")
-                            self.colored_points.append(point)
+                self.check_first_and_last_window(vis, old_points, line, min_x, max_x, min_y, max_y)
 
-        min_x, max_x = (vis.polygon_dots[0].x, vis.polygon_dots[1].x)
-        min_y, max_y = (vis.polygon_dots[0].y, vis.polygon_dots[1].y)
-        if vis.polygon_dots[0].x > vis.polygon_dots[1].x:
-            min_x, max_x = max_x, min_x
-        if vis.polygon_dots[0].y > vis.polygon_dots[1].y:
-            min_y, max_y = max_y, min_y
+            if ceil(min_y / self.windows_width) != min_y / self.windows_width:
+                self.check_first_or_last_line(floor(min_y / self.windows_width),
+                                              vis, old_points, min_x, max_x, min_y, max_y)
+
+            if floor(max_y / self.windows_width) != max_y / self.windows_width:
+                self.check_first_or_last_line(floor(max_y / self.windows_width),
+                                              vis, old_points, min_x, max_x, min_y, max_y)
+
+            for point in old_points:
+                vis.canv.itemconfig(point, outline="#000000")
+
+        x1, x2 = (vis.polygon_dots[0].x, vis.polygon_dots[1].x)
+        y1, y2 = (vis.polygon_dots[0].y, vis.polygon_dots[1].y)
+        min_x, max_x = (x1, x2) if x1 < x2 else (x2, x1)
+        min_y, max_y = (y1, y2) if y1 < y2 else (y2, y1)
 
         return action
 
