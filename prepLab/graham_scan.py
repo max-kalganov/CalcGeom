6143b0d54ff77e71ea048ff3bedbdfde0a74a892
max <maksim.kalganov@kreo.net>
2020-04-08 13:11:11 +0300

    finish graham scan

--- prepLab/graham_scan.py
+++ prepLab/graham_scan.py
@@ -1,9 +1,9 @@
-from typing import Tuple
+from typing import Tuple, Optional, Iterable, Callable
 
 import numpy as np
 from sortedcontainers import SortedList
+from geompreds import orient2d
 
-from Models import Dot
 from ct import CANVAS_WIDTH, CANVAS_HEIGHT, DEFAULT_NUM_OF_POINTS
 from utils import dist, dist_between_points
 from visualizer import SimpleVisualizer
@@ -11,21 +11,37 @@ from visualizer import SimpleVisualizer
 
 class GrahamScan:
     def __init__(self):
-        self.all_points = None
+        self.all_points: Optional[SortedList] = None
+        self.conv = []
+        self.start_point: Optional[Tuple[int, int]] = None
+        self.cur_index: Optional[int] = None
+
+    def full_init(self, vis: SimpleVisualizer):
+        self.__init__()
+        self.init_field(vis)
+
+    def _tan(self, point: Tuple[float, float]) -> Tuple[float, float]:
+        if point == self.start_point:
+            return 0, 0
+        if point[0] == self.start_point[0] and point[1] != self.start_point[1]:
+            return (1 if point[1] > self.start_point[1] else -1) * np.inf, point[1]
+        return (point[1] - self.start_point[1]) / (point[0] - self.start_point[0]), dist_between_points(self.start_point, point)
 
     def init_field(self, vis: SimpleVisualizer):
-        def tan(point1: Tuple[int, int], point2: Tuple[int, int]) -> Tuple[float, float]:
-            if point2[0] == point1[0] and point2[1] != point1[1]:
-                return (1 if point2[1] > point1[1] else -1)*np.inf, point2[1]
-            return (point2[1] - point1[1])/(point2[0] - point1[0]), dist_between_points(point1, point2)
         seen = {}
         x_values = np.random.normal(loc=CANVAS_WIDTH // 2, scale=CANVAS_WIDTH // 8, size=DEFAULT_NUM_OF_POINTS)
         y_values = np.random.normal(loc=CANVAS_HEIGHT // 2, scale=CANVAS_HEIGHT // 8, size=DEFAULT_NUM_OF_POINTS)
+        x_values = x_values[(x_values >= 0) & (x_values <= CANVAS_WIDTH)]
+        y_values = y_values[(y_values >= 0) & (y_values <= CANVAS_HEIGHT)]
+        min_len = min(len(x_values), len(y_values))
+        x_values = x_values[:min_len]
+        y_values = y_values[:min_len]
 
         min_x_ind = np.argmin(x_values)
-        self.all_points = SortedList(zip(x_values, y_values),
-                                     key=lambda point: tan((x_values[min_x_ind], y_values[min_x_ind]),
-                                                           point))
+        self.start_point = x_values[min_x_ind], y_values[min_x_ind]
+        self.all_points = SortedList(zip(x_values[x_values != self.start_point[0]],
+                                         y_values[y_values != self.start_point[1]]),
+                                     key=self._tan)
 
         for x, y in zip(x_values, y_values):
             if x in seen:
@@ -38,11 +54,46 @@ class GrahamScan:
             vis.canv.create_rectangle((x, y) * 2)
 
     def action(self, vis: SimpleVisualizer):
-        pass
+        self.gen_conv()
+        self.draw_conv(vis)
 
+    def draw_conv(self, vis: SimpleVisualizer):
+        def divided_coords() -> Iterable[int]:
+            for point in self.conv:
+                yield point[0]
+                yield point[1]
+
+        vis.canv.create_line(*divided_coords())
+
+    def gen_conv(self):
+        def get_next():
+            assert self.cur_index <= len(self.all_points)
+            if self.cur_index == len(self.all_points):
+                el = self.start_point
+            else:
+                el = self.all_points[self.cur_index]
+            self.cur_index += 1
+            return el
+
+        def is_r_turn(p1: Tuple[float, float], p2: Tuple[float, float], p3: Tuple[float, float]) -> bool:
+            return orient2d(p1, p2, p3) <= 0
+
+        self.cur_index = 0
+        self.conv.append(self.start_point)
+        v = get_next()
+        w = get_next()
+        while w != self.start_point:
+            if is_r_turn(self.conv[-1], v, w):
+                v = self.conv.pop()
+            else:
+                self.conv.append(v)
+                v = w
+                w = get_next()
+        self.conv.append(v)
+        self.conv.append(self.start_point)
 
 if __name__ == '__main__':
     alg = GrahamScan()
-    v = SimpleVisualizer(init_func=alg.init_field,
+    v = SimpleVisualizer(init_func=alg.full_init,
                          action_func=alg.action)
     v.run_loop()
--- prepLab/utils.py
+++ prepLab/utils.py
@@ -7,6 +7,19 @@ def dist(a):
     return np.sqrt(a[0] ** 2 + a[1] ** 2)
 
 
-def dist_between_points(p1: Tuple[int, int], p2: Tuple[int, int]) -> float:
+def dist_between_points(p1: Tuple[float, float], p2: Tuple[float, float]) -> float:
     diff = p2[0] - p1[0], p2[1] - p1[1]
     return dist(diff)
+
+
+def binsearch(points, key, val: Tuple[float, float]):
+    low: int = 0
+    high: int = len(points) - 1
+    while low < high:
+        mid = (low + high) // 2
+        if key(points[mid]) < key(val):
+            low = mid + 1
+        else:
+            high = mid
+    return low if points[low] == val else -1
+
